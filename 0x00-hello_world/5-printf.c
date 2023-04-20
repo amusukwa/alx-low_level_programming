2c5944b2b35b7bddf3253b81b606d4294d5b1b05
amusukwa <amusukwa@gmail.com>
2023-04-20 04:36:42 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:A program that uses the printf function
+ *
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
