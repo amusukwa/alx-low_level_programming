b572c297245e2e9fc33420841da7468f75aa1cb3
amusukwa <amusukwa@gmail.com>
2023-04-20 09:39:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
 /**
  * main - Entry point
  *
- * Return: Always (1) (Successful)
+ * Return: Always 1 (Successful)
  */
 int main(void)
-{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
