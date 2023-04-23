444bcf1ee5f6d9868c641376be08dca49902ee53
amusukwa <amusukwa@gmail.com>
2023-04-23 04:44:01 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/** 
+/**
  * main - Entry point
  *
  * Description - a program that uses the if else statement
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive")
-			if (n == 0)
-				printf("%d is zero")
+		printf("%d is positive\n", n)
+			else if (n == 0)
+				printf("%d is zero\n", n)
 			else
-				printf("%d is negatve")
+				printf("%d is negatve\n", n)
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n > 5)
+		printf("and is greater than 5")
+			if (n ==0)
+				printf("is zero")
+			else
+				printf("and is less than 6 and not 0")
 	return (0);
 }
