2b2d4a989709bfc4de33d98b27af5c30faa452d3
amusukwa <amusukwa@gmail.com>
2023-04-25 05:10:48 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add()- adds two integers and returns the result
+ *
+ * @a: firstinput members as integers
+ * @b: second input as integer
+ *
+ * Return: integer value
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
