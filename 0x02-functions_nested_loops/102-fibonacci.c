f479c2765164eb2ee449a98c5fbc09b6deaeb117
amusukwa <amusukwa@gmail.com>
2023-04-25 13:25:31 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,16 +13,16 @@ int main(void)
 	while (i < 50)
 	{
 		if (i == 0)
-			printf("%ld", j);
-			else if (i == 1)
-				printf(", %ld ", k);
-				else
-				{
-					k += j;
-					j = k - j;
-					printf(", %ld", k);
-				}
-				++i;
+		printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld ", k);
+			else
+			{
+				k += j;
+				j = k - j;
+				printf(", %ld", k);
+			}
+			++i;
 	}
 	printf("\n");
 	return (0);
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -23,4 +23,3 @@ int main(void)
 	printf("%ld\n", sum);
 	return (0);
 }
-
