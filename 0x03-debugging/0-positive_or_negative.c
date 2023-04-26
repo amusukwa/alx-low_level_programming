8678054b96f5af06fbe26c81730a4aa4c5c2ab15
amusukwa <amusukwa@gmail.com>
2023-04-26 03:11:44 -0700

    0-main.c

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - a function that tests negative or positive 
+ *
+ *Return: no return value
+ */
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
