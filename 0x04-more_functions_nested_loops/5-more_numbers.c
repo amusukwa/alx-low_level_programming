29b45ca51a5f9ae89558aca1120cd1709ead85aa
amusukwa <amusukwa@gmail.com>
2023-04-27 12:26:10 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,14 @@
  * more_numbers - prints the numbers, from 0 to 9 10 times
  *
  * Return: no return type
+ *
  */
 void more_numbers(void)
 {
 	int x, y;
-	
+
 	for (x = 0; x < 10; x++)
-	{	
+	{
 		for (y = 0; y <= 14; y++)
 		{
 			if (y > 9)
@@ -19,6 +20,6 @@ void more_numbers(void)
 			}
 			_putchar((y % 10) + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
