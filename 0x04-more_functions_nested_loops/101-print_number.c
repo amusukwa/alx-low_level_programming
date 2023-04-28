712292d94e30f625b86973dd5068681ba23868c7
amusukwa <amusukwa@gmail.com>
2023-04-28 10:54:04 -0700

    101-main.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,15 +11,13 @@
 void print_number(int n)
 {
 unsigned int d = n;
-if (n < 0) 
+if (n < 0)
 {
 _putchar('-');
 d = -d;
- 
 }
 
 if ((d / 10) > 0)
-print_number( d  / 10 );
-
-_putchar(d %10 + '0');
+print_number(d  / 10);
+_putchar(d % 10 + '0');
 }
