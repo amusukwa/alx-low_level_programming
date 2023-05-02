ede20d8c22491a839ee5197d5a7e2998236eddf1
amusukwa <amusukwa@gmail.com>
2023-05-02 00:05:34 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: first input as integer
+ * @b: second input as integer
+ * Return:no return type
+ */
+void swap_int(int *a, int *b)
+{
+	m = *a;
+
+	*a = *b;
+	*b = m;
+}
