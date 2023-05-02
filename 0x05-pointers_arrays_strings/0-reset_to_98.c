be58c75e93dbb35e2b3f35842b25c69f303ec1ed
amusukwa <amusukwa@gmail.com>
2023-05-01 23:51:11 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,10 @@
 /**
  * reset_to_98 -  takes a pointer to an int as parameter and updates
  *  the value it points to to 98
- *  @i:input as integer
+ *  @n:input as integer
  *  Return:no return type
  */
 void reset_to_98(int *n)
 {
+	*n = 98;
 }
