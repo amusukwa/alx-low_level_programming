a175103c2a97b4578e71bf5fb9bb276be15dbe3c
amusukwa <amusukwa@gmail.com>
2023-05-04 00:06:55 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strcat -  concatenates two strings
+ * @dest:first input  parameter as char
+ * @src:second input as char
+ * Return:return concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+
