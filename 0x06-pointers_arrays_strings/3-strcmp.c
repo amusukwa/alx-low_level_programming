bfbb6788e2ea70f39722547fce8a7f93b94f9921
amusukwa <amusukwa@gmail.com>
2023-05-04 04:04:53 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _strcmp - compares a string
+ * _strcmp - compares string values
  * @s1:first input parameter
  * @s2:second input parameter
  * Return: 0 (success)
  */
-char *_strncpy(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int i;
 
 i = 0;
-while (dest[i] != '\0' && src[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
 return  (s1[i] - s2[i]);
 }
-return (0);
+i++;
 }
+return (0);
 }
