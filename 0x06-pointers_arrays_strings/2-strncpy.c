3d41519324ea92fc3a503710a8d4699cca3a6654
amusukwa <amusukwa@gmail.com>
2023-05-04 10:37:49 -0700

    2-main.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- * _strncat -  copy a string
- * @dest:first input as char
- * @src:second input as char
- * @n:third input as integer
- * Return: concatenated string
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+	int j;
 
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
 }
