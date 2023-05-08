0f5e53b713d184eda5d8d3f3c08fc83c567e2b3e
amusukwa <amusukwa@gmail.com>
2023-05-08 07:52:13 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _strspn - gets the length of a prefix substring
+ *  _strspn - gets the length of a prefix substring
  * @s:input string character
  * @accept:substring of string
  * Return:number of bytes
