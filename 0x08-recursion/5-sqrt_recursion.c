67209ba949400446f7c46f6e68896473f97808e3
amusukwa <amusukwa@gmail.com>
2023-05-10 05:02:46 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n:input as integer
+ * Return:returns integer
+ */
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+/**
+ * _sqrt -calculates natural square root
+ * @n:number to calculte square root
+ * @i:number to iterate
+ * Return:natural square root
+ */
+int _sqrt(int n, int i)
+{
+int sqrt = i * i;
+if (sqrt > n)
+return (-1);
+if (sqrt == n)
+return (i);
+return (_sqrt(n, i + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number, otherwise return 0
+ * @n:input integer
+ * Return:return 1 if prime number,otherwise 0
+ */
+int is_prime_number(int n)
+{
+
