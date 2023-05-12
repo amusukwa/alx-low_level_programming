6c4aacc2add317f40a01da6adf91856328070cd3
amusukwa <amusukwa@gmail.com>
2023-05-12 01:30:33 -0700

    main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Deescription - prints first argument
+ * Return: 0 Always(success)
+ */
+int main(void)
+{
+       puts(argv[0]);
+     
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ This directory contains solutions for the 0x0A. C - argc, argv project
