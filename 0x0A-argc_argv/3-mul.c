6640dc6efcb054440a909850186033f0e64fc72e
amusukwa <amusukwa@gmail.com>
2023-05-12 06:26:00 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Prints argument number
+ * main - multiples two numbers
  * @argc:argument count
  * @argv:argument vector
  * Return: 0 Always(success)
  */
 int main(int argc, char *argv[])
 {
-(void) argv;
-printf("%d\n", argc - 1);
-return (0);
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);		
+printf("%d\n", a * b);
 }
-~                                                                                                                                                    
-~             
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}                                                                                                   
