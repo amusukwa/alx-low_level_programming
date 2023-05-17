1ed2a2027b7aedfa61d1d110138d9eb7b4ef7b8b
amusukwa <amusukwa@gmail.com>
2023-05-17 03:56:04 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,7 +25,7 @@ return (w);
 /**
  * strtow - splits a string into words
  * @str: string to split
- * Return: pointer to an array of strings (Success)             
+ * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
 char **strtow(char *str)
