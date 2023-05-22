6d1f4103ef9246c68d2bab4fb6d8d120d47e5145
amusukwa <amusukwa@gmail.com>
2023-05-22 01:15:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a struct that has a dogs info
+ * @name:first member
+ * @age:second member
+ * @owner:third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
