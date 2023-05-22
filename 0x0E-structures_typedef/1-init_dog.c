0bbb73439084810c4df8320a12bb8fa89038e9c1
amusukwa <amusukwa@gmail.com>
2023-05-22 02:02:19 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,19 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * struct dog - a struct that has a dogs info
+ * @name:first member
+ * @age:second member
+ * @owner:third member
+ * Return:no return type
  */
-int main(void)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog my_dog;
-init_dog(&my_dog, "Poppy", 3.5, "Bob");
-printf("My name is %s, and I am %.1f :) - Woof|\n", my_dog.name, my_dog.age);
-return (0);
+if ( d != NULL)
+{
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
+}
 }
