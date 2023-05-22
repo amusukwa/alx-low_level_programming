36e324e8b6a4bf8be23fb550adf387e934813ee8
amusukwa <amusukwa@gmail.com>
2023-05-22 10:45:06 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,13 @@
  */
 void print_dog(struct dog *d)
 {
-if (d != NULL)
-{
-printf("Name: %s\n", (d->name) ? d->name : ("nil"));
-printf("Age: %.6f\n", (d->age) ? d->age : 0);
-printf("Ownwer: %s\n", (d->owner) ? d->owner : ("nil"));
-}
+if (d == NULL)
+return;
+
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
