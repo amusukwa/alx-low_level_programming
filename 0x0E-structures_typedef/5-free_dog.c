dcce60ef666a5358a868b056134e3f6ef89d6161
amusukwa <amusukwa@gmail.com>
2023-05-22 06:30:27 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dog
+ * @d:...
+ * Return:...
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
