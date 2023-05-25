9359a1bce2043dbeaad142fd5ccd9838b60e665c
amusukwa <amusukwa@gmail.com>
2023-05-25 09:22:28 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,45 +10,43 @@
 void print_all(const char * const format, ...)
 {
 va_list valist;
-unsigned int i = 0, j = 0, c = 0;
+unsigned int i = 0, j, c = 0;
 char *str;
 const char t_args[] = "cifs";
 va_start(valist, format);
-
-while 9(format && format[i])
+while (format && format[i])
 {
 j = 0;
 while (t_args[j])
 {
 if (t_args[i] == format[j] && c)
 {
-printf(", ");
+printf(",");
 break;
 }
 j++;
 }
-switch (format[j])
+switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+printf("%c", va_arg(valist, int)),c = 1;
 break;
 case 'i':
 printf("%d", va_arg(valist, int)), c = 1;
 break;
 case 'f':
-printf("%f", va_arg(valist, int)), c = 1;
+printf("%f", va_arg(valist, double)), c = 1;
 break;
 case 's':
-str = va_arg(valist, char *), c = 1;
+str = va_arg(valist, char *),c = 1;
 if (!str)
 {
 printf("(nil)");
 break;
 }
 printf("%s", str);
-}
-i++;
+} i++;
 printf("\n");
-va_end(valist;
+va_end(valist);
 }
 }
