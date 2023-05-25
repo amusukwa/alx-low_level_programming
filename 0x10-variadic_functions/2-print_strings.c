4af1b18cf8648475b529054a148d912434f59adc
amusukwa <amusukwa@gmail.com>
2023-05-25 11:52:08 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,23 +10,12 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
-unsigned int i;
-char *str;
 
-va_start(valist, n);
-
+va_list ap;
+unsigned int i, sum = 0;
+va_start(ap, n);
 for (i = 0; i < n; i++)
-{
-str = va_arg(valist, char *);
-if (str)
-printf("%s", str);
-else
-printf("( nil)");
-if (i < n - 1)
-if (separator)
-printf("%s", separator);
-}
-printf("\n");
-va_end(valist);
+sum += va_arg(ap, int);
+va_end(ap);
+return (sum);
 }
