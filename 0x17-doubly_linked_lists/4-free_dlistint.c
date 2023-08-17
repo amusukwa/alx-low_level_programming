17fdf0726cf712c602082e12825149527caf241f
amusukwa <amusukwa@gmail.com>
2023-08-17 11:53:10 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint- This frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_dlistint(dlistint_t *head)
+{
+
+dlistint_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+
+}
