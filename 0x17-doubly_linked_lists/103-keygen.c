7dc2418137b492cd8ca9c278a341d1f8bfc76c04
amusukwa <amusukwa@gmail.com>
2023-08-18 09:20:00 -0700

    103-keygen.c betty

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -10,13 +10,13 @@
 int main(void)
 {
 
-char a[10], c[10], d;
+char a[10], c[10];
 int i, j, k = 0;
 
 printf("#Keygen by b44nz0r\n\n");
+
 while (k < 5 || k >= 10)
-
-
+{
 if (k != 0)
 printf("\nThe username length should be 5 to 10 alphabets\n");
 printf("enter username: ");
@@ -26,7 +26,6 @@ k = strlen(a);
 
 i = k - 1;
 j = 0;
-
 while (i >= 0)
 {
 c[j] = a[i] + i;
@@ -38,4 +37,7 @@ printf("\nThe password is %s\n", c);
 printf("\nHit Enter to Exit\n");
 getchar();
 getchar();
+
+return (0);
 }
+
