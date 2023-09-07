ce22af783f3bb73ccd2ae88f5a3388fc3408f84d
amusukwa <amusukwa@gmail.com>
2023-09-07 10:00:22 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,39 +3,44 @@
 #include <string.h>
 #include "hash_tables.h"
 
-#define HASH_TABLE_SIZE 1024  
+#define HASH_TABLE_SIZE 1024
+
 /**
  * hash_table_create - creates hash table
  * @size:input parameter
  * Return:returns hash table
  */
 
-
-HashTable *hash_table_create(size_t size)
+hash_table_t *hash_table_create(size_t size)
 {
-    HashTable *table = NULL;
-    size_t i;
-    table = (HashTable *)malloc(sizeof(HashTable));
-    if (table == NULL)
-    {
-        return NULL; 
-    }
 
-    
-    table->size = size;
-    table->array = (HashNode **)malloc(sizeof(HashNode *) * size);
-    if (table->array == NULL)
-    {
-        free(table); 
-        return NULL;
-    }
+hash_table_t *table = NULL;
+size_t i;
+table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-    for (i = 0; i < size; i++) {
-        table->array[i] = NULL;
-    }
-
-    return table;
+if (table == NULL)
+{
+return (NULL);
 }
+
+table->size = size;
+table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+
+if (table->array == NULL)
+{
+free(table);
+return (NULL);
+}
+
+for (i = 0; i < size; i++)
+{
+table->array[i] = NULL;
+}
+return (table);
+}
+
+
+
 
 
 
