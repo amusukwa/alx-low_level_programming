0e32146b222375e231d6f0054da22d71ef6cd68f
amusukwa <amusukwa@gmail.com>
2023-09-08 02:37:13 -0700

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,16 +10,13 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-    unsigned long int index;
 
-    if (key == NULL || size == 0)
-        return (0);
+unsigned long int hash_value;
+unsigned long int index;
 
-    hash_value = hash_djb2(key);
-
-   
-    index = hash_value % size;
-
-    return (index);
+if (key == NULL || size == 0)
+return (0);
+hash_value = hash_djb2(key);
+index = hash_value % size;
+return (index);
 }
