fdc633679ba3b54723ac2f8f8a28b8bada60e709
amusukwa <amusukwa@gmail.com>
2023-12-13 00:33:17 -0800

    betty error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,19 +11,19 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
-if (array == NULL)
-{
-return (-1);
-}
-for(i = 0; i < size; i++)
-{
-printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	size_t i;
 
-if (array[i] == value)
-{
-return (i);
-}
-}
-return (-1);
+	for (i = 0; (i < size) && (array); i++)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		}
+	}
+	return (-1);
 }
